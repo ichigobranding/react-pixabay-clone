37335fde14c292cf9fb3a93c085bd8e246760939
Soichiro Tanaka <tanaka-pc@SoichironoMacBook-Pro-2.local>
2022-11-27 00:46:15 +0900

    api設定完了

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import ImageGrallery from './ImageGrallery';
 
+//https://github.com/Shin-sibainu/my-pixabay-with-react/tree/main/src
+
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+    //APIを叩く(データフェッチ)
+    //API URL
+    const endpointUrl = `https://pixabay.com/api/?key=31621166-d69c50502e9ef7b3bd158230b&q=${ref.current.value}&image_type=photo&pretty=true`;
+    fetch(endpointUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        //console.log(data.hits);
+        setFetchData(data.hits);
+      });
+  }
+
   return (
     <div className="container">
       <h2>PixaBay</h2>
-      <form>
-        <input type="text" placeholder="画像を検索"/>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="画像を探す" ref={ref} />
       </form>
-      <ImageGrallery />
+      <ImageGrallery fetchData={fetchData}/>
     </div>
   );
 }
--- src/ImageGrallery.js
+++ src/ImageGrallery.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./ImageGrallery.css";
 
-const ImageGrallery = () => {
+const ImageGrallery = ({ fetchData }) => {
+  //endpointURL: https://pixabay.com/api/?key=31621166-d69c50502e9ef7b3bd158230b&q=yellow&image_type=photo&pretty=true
   return (
     <div>
-      ImageGrallery
+      <div className="images-wrapper">
+      {fetchData.map((data) => (
+          <div className="image" key={data.id}>
+            <a href={data.pageURL} target="_blank">
+              <img src={data.largeImageURL} alt="" />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
